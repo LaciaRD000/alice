aeef3c70cec12068095d7c533e67e8238ad0588d
Xerado <116932901+LaciaRD000@users.noreply.github.com>
2024-03-22 19:29:53 +0900

    コマンド更新

--- internal/commands/help.go
+++ internal/commands/help.go
@@ -24,20 +24,44 @@ func HelpHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 					Value: "Spam対策をします。",
 				},
 				{
+					Name:  "/ban",
+					Value: "対象のユーザーをサーバーからBanします。",
+				},
+				{
+					Name:  "/disconnect",
+					Value: "参加しているVCから切断します",
+				},
+				{
 					Name:  "/play",
 					Value: "voice channelに参加し、音楽を流してくれます。",
 				},
 				{
+					Name:  "/reaction-panel",
+					Value: "リアクションパネルを設置します。",
+				},
+				{
 					Name:  "/shop",
-					Value: "半自動販売機を設置してくれます",
+					Value: "半自動販売機を設置します。",
+				},
+				{
+					Name:  "/spam",
+					Value: "指定されたメッセージをSpamします。",
+				},
+				{
+					Name:  "/stop",
+					Value: "spamを止めます。",
 				},
 				{
 					Name:  "/ticket",
-					Value: "チケットパネルを設置してくれます。",
+					Value: "チケットパネルを設置します。",
 				},
 				{
 					Name:  "/verify",
-					Value: "認証パネルを設置してくれます。",
+					Value: "認証パネルを設置します。",
+				},
+				{
+					Name:  "/welcome",
+					Value: "サーバー参加時にメッセージを送信する設定を変更できます。",
 				},
 			},
 		},
